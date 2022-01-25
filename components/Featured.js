869d58cfe73bd60f57763f45387b2cb3703de1ca
jewoo <jeus0630@designfever.com>
2022-01-25 19:13:07 +0900

    Style : Main slider

--- components/Featured.js
+++ components/Featured.js
@@ -1,9 +1,31 @@
-import styles from '/styles/Featured.moduel.scss';
+import styles from '/styles/Featured.module.scss';
+import Image from "next/image";
 
 const Featured = () => {
-    return (
-        <div>
 
+    const images = [
+        "/img/featured.png",
+        "/img/featured2.png",
+        "/img/featured3.png",
+    ]
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.arrowContainer} style={{left: 0}}>
+                <Image src={"/img/arrowl.png"} layout={"fill"}></Image>
+            </div>
+            <div className={styles.wrapper}>
+                {
+                    images.map((image, idx) => (
+                        <div className={styles.imgContainer} key={idx}>
+                            <Image src={image} layout="fill"></Image>)
+                        </div>
+                    ))
+                }
+            </div>
+            <div className={styles.arrowContainer} style={{right: 0}}>
+                <Image src={"/img/arrowr.png"} layout={"fill"}></Image>
+            </div>
         </div>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Featured from "../components/Featured";
 export default function Home() {
   return (
-    <div className={styles.container}>
-        <h1>Hello World</h1>
+    <div>
+        <Featured></Featured>
     </div>
   )
 }
